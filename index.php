13147b5b5e957d1cbdf1d2b055e4107fb1e01380
Vultra <30537108+Vultra@users.noreply.github.com>
2017-07-28 21:57:45 +0100

    index.php

---
+++ index.php
@@ -0,0 +1,194 @@
+<!--
+_________                        __             .___
+\_   ___ \_______   ____ _____ _/  |_  ____   __| _/
+/    \  \/\_  __ \_/ __ \\__  \\   __\/ __ \ / __ |
+\     \____|  | \/\  ___/ / __ \|  | \  ___// /_/ |
+ \______  /|__|    \___  >____  /__|  \___  >____ |
+        \/             \/     \/          \/     \/
+__________                                      
+\______   \___.__.                              
+ |    |  _<   |  |                              
+ |    |   \\___  |                              
+ |______  // ____|                              
+        \/ \/                                    
+____   ____    .__   __                          
+\   \ /   /_ __|  |_/  |_____________            
+ \   Y   /  |  \  |\   __\_  __ \__  \          
+  \     /|  |  /  |_|  |  |  | \// __ \_        
+   \___/ |____/|____/__|  |__|  (____  /        
+                                     \/
+
+                                     -->
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PS4Extreme</title>
+
+        <!-- javascript -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+        <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+        <!-- fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+        <!-- css -->
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+                font-weight: bold;
+                color: #ffffff
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .logo {
+                display: none;
+            }
+
+            .title {
+                font-size: 96px;
+                display: none;
+            }
+
+            .moto {
+                font-size: 22px;
+                display: none;
+            }
+
+            .downloadinfo {
+                display: none;
+            }
+
+            .footer {
+                position: absolute;
+                right: 0;
+                bottom: 0;
+                left: 0;
+                 height: 5%;
+                 color: #ffffff;
+                 background-color: #000000;
+                 text-align: center;
+                 display: none;
+                }
+
+                * { box-sizing: border-box; }
+.video-background {
+  background: #000;
+  position: fixed;
+  top: 0; right: 0; bottom: 0; left: 0;
+  z-index: -99;
+}
+.video-foreground,
+.video-background iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  pointer-events: none;
+}
+#vidtop-content {
+    top: 0;
+    color: #fff;
+}
+.vid-info { position: absolute; top: 0; right: 0; width: 33%; background: rgba(0,0,0,0.3); color: #fff; padding: 1rem; font-family: Avenir, Helvetica, sans-serif; }
+.vid-info h1 { font-size: 2rem; font-weight: 700; margin-top: 0; line-height: 1.2; }
+.vid-info a { display: block; color: #fff; text-decoration: none; background: rgba(0,0,0,0.5); transition: .6s background; border-bottom: none; margin: 1rem auto; text-align: center; }
+@media (min-aspect-ratio: 16/9) {
+  .video-foreground { height: 300%; top: -100%; }
+}
+@media (max-aspect-ratio: 16/9) {
+  .video-foreground { width: 300%; left: -100%; }
+}
+@media all and (max-width: 600px) {
+.vid-info { width: 50%; padding: .5rem; }
+.vid-info h1 { margin-bottom: .2rem; }
+}
+@media all and (max-width: 500px) {
+.vid-info .acronym { display: none; }
+}
+        </style>
+    </head>
+    <body>
+    <div class="video-background">
+    <div class="video-foreground">
+      <iframe src="https://www.youtube.com/embed/8LWZSGNjuF0?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=8LWZSGNjuF0" frameborder="0" allowfullscreen></iframe>
+    </div>
+  </div>
+    <div class="container">
+    <div class="content">
+    <div class="logo"><img src="https://www.psxhax.com/styles/default/xenforo/logo.png"></div>
+                <div class="title">PS4Extreme</div>
+               <div class="moto"> ~ A Porno away from a Jailbreak ~ </div>
+
+               <!-- Trigger the modal with a button -->
+               <div class="downloadinfo">
+               <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#download">Download</button>
+               </div>
+               <!-- Modal -->
+               <div id="download" class="modal fade" role="dialog">
+               <div class="modal-dialog">
+               <!-- Modal content-->
+               <div class="modal-content">
+               <div class="modal-header">
+               <button type="button" class="close" data-dismiss="modal">&times;</button>
+               <font color="black">
+               <h4 class="modal-title">PS4Extreme</h4>
+               </font>
+               </div>
+               <div class="modal-body">
+               <font color="black">
+               <p>Welcome to PS4Extreme RTM Tool By Vultra.</p>
+               </font>
+               </div>
+               <div class="modal-footer">
+               <button type="button" class="btn btn-secondary" data-dismiss="modal" aria-hidden="true">Close</button>
+               <a href="https://vultra.github.io/PS4Extreme.rar" role="button" class="btn btn-primary">Download</a>
+               </div>
+               </div>
+               </div>
+               </div>
+
+               </div>
+        </div>
+     
+        <div class="footer">
+        <p><script type="text/javascript"> document.write(new Date().getFullYear());</script>&copy; <a href="https://www.psxhax.com/members/wultra.560/">Vultra </a></p>
+        </div>
+
+    </body>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $(".logo").fadeIn(2500);
+            $(".title").fadeIn(4500);
+            $(".moto").fadeIn(6500);
+            $(".downloadinfo").fadeIn(8500);
+            $(".footer").fadeIn(8500);
+        });
+    </script>
+</html>
